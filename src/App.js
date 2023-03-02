33ea22cc70dfb35c7355a1076e6abad2e0590f82
nex-DoT <nex.dot2@gmail.com>
2023-03-02 17:50:33 +0330

    responsive menu react traning -v1.0

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import data, { links, soials } from './data';
 import {AiOutlineMenu}from 'react-icons/ai';
 
 function App() {
+  const [show,setShow] = useState(false);
   return (<>
        <nav className='nav'>
-        <div className='open'><button><AiOutlineMenu color='#00ffffae' fontSize='20px'/></button></div>
+        <div className='open'><button onClick={()=> setShow(!show)}><AiOutlineMenu color='#00ffffae' fontSize='20px'/></button></div>
 
          <div className='menu'>
           <ul>
@@ -25,14 +26,15 @@ function App() {
           </ul>
          </div>
        </nav>
-               <div className='menu1'>
-               <ul>
-                   {links.map((link)=>{
-                     const {id,url,text}=link;
-                     return <li key={id}><a href={url}>{text}</a></li>
-                   })}
-                 </ul>
-               </div>
+                <div className={`${show ? 'menu1 active' : 'menu1'}`}>
+                <ul>
+                    {links.map((link)=>{
+                      const {id,url,text}=link;
+                      return <li key={id}><a href={url}>{text}</a></li>
+                    })}
+                  </ul>
+                </div>
+              
                </>
 
   );
